8e2dc1160e8582335f6e946b402dd950bb78ac5a
André Brás <andrebras@gmail.com>
2014-05-03 12:03:24 +0100

    Custom ruby alias

--- modules/ruby/init.zsh
+++ modules/ruby/init.zsh
@@ -53,16 +53,9 @@ alias rb='ruby'
 # Bundler
 if (( $+commands[bundle] )); then
   alias rbb='bundle'
-  alias rbbe='bundle exec'
-  alias rbbi='bundle install --path vendor/bundle'
-  alias rbbl='bundle list'
-  alias rbbo='bundle open'
-  alias rbbp='bundle package'
-  alias rbbu='bundle update'
-  alias rbbI='rbbi \
-    && bundle package \
-    && print .bundle       >>! .gitignore \
-    && print vendor/assets >>! .gitignore \
-    && print vendor/bundle >>! .gitignore \
-    && print vendor/cache  >>! .gitignore'
+  alias be='rbb exec'
+  alias bi='rbb install'
+  alias bl='rbb list'
+  alias bo='rbb open'
+  alias bu='rbb update'
 fi
