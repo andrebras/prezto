c48afd3e4b18c1232d959f6bfcd349baa2818691
André Brás <andrebras@gmail.com>
2014-05-03 12:03:05 +0100

    Custom rails alias

--- modules/rails/init.zsh
+++ modules/rails/init.zsh
@@ -19,18 +19,10 @@ fi
 # Aliases
 #
 
-alias ror='bundle exec rails'
-alias rorc='bundle exec rails console'
-alias rordc='bundle exec rails dbconsole'
-alias rordm='bundle exec rake db:migrate'
-alias rordM='bundle exec rake db:migrate db:test:clone'
-alias rordr='bundle exec rake db:rollback'
-alias rorg='bundle exec rails generate'
-alias rorl='tail -f "$(ruby-app-root)/log/development.log"'
-alias rorlc='bundle exec rake log:clear'
-alias rorp='bundle exec rails plugin'
-alias rorr='bundle exec rails runner'
-alias rors='bundle exec rails server'
-alias rorsd='bundle exec rails server --debugger'
-alias rorx='bundle exec rails destroy'
-
+alias ra='bundle exec rails'
+alias rac='bundle exec rails console'
+alias ras='bundle exec rails server'
+alias rag='bundle exec rails generate'
+alias rad='bundle exec rails destroy'
+alias ral='tail -f "$(ruby-app-root)/log/development.log"'
+alias ralc='bundle exec rake log:clear'
